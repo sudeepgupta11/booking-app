97f20854021183f53a370eceae4e94b68456906d
sudeepgupta11 <sudeepgupta11@gmail.com>
2022-04-10 15:03:37 +0530

    Updating import statement

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -7,7 +7,6 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { createTheme } from "@mui/material/styles";
 import { AppContext } from "./../App";
 
 const StyledTableCell = styled(TableCell)(() => ({
